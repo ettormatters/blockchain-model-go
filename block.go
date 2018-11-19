4e9e736be55c7c23b9d5d3239fd1fe290191df54
oeritas <advanwith@gmail.com>
2018-11-19 22:20:44 +0900

    refac

---
+++ block.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+	"strconv"
+	"time"
+)
+
+//Block keeps block headers
+type Block struct {
+	Timestamp int64
+	Data      []byte
+	PrevBlock []byte
+	Hash      []byte
+}
+
+//SetHash calculates and sets block hash
+func (b *Block) SetHash() {
+	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+	headers := bytes.Join([][]byte{b.PrevBlock, b.Data, timestamp}, []byte{})
+	fmt.Printf("%s\n", headers)
+	hash := sha256.Sum256(headers)
+	b.Hash = hash[:]
+}
+
+//NewBlock creates and returns Block
+func NewBlock(data string, prevBlock []byte) *Block {
+	block := &Block{time.Now().Unix(), []byte(data), prevBlock, []byte("")}
+	block.SetHash()
+	return block
+}
+
+//NewGenesisBlock creates and returns genesis Block
+func NewGenesisBlock() *Block {
+	return NewBlock("Genesis Block", []byte("0"))
+}
---
+++ blockchain.go
@@ -0,0 +1,21 @@
+package main
+
+import "time"
+
+//Blockchain keeps a sequence of Blocks
+type Blockchain struct {
+	blocks []*Block
+}
+
+//AddBlock saves provided data as a block in the blockchain
+func (bc *Blockchain) AddBlock(data string) {
+	prevBlock := bc.blocks[len(bc.blocks)-1]
+	newBlock := &Block{time.Now().Unix(), []byte(data), prevBlock.Hash, []byte("")}
+	newBlock.SetHash()
+	bc.blocks = append(bc.blocks, newBlock)
+}
+
+//NewBlockchain creates a new ..
+func NewBlockchain() *Blockchain {
+	return &Blockchain{[]*Block{NewGenesisBlock()}}
+}
