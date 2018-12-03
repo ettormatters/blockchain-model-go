ca85875e0f539273184d26a8e3841f7b89670f2b
oeritas <advanwith@gmail.com>
2018-12-03 23:54:23 +0900

    transaction part

--- main.go
+++ main.go
@@ -1,32 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
-
 func main() {
-	bc := NewBlockchain()
-	defer bc.db.Close()
-
-	// bc.AddBlock("Send 1 BTC to Ivan")
-	// bc.AddBlock("Send 2 more BTC to Ivan")
-
-	bci := bc.Iterator()
-
-	for {
-		block := bci.Next()
-
-		fmt.Printf("%s - %x\n", block.Data, block.Hash)
-		fmt.Printf("Prev. hash: %s\n", block.PrevBlockHash)
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %x\n", block.Hash)
-		pow := NewProofOfWork(block)
-		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
-		fmt.Println()
-
-		if len(block.PrevBlockHash) == 0 {
-			break
-		}
-	}
+	cli := CLI{}
+	cli.Run()
 }
